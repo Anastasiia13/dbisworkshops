d1e43dc699014e936a703ea523bef83c71752d5c
Anastasiia13 <nastyavozniak@gmail.com>
2018-11-26 09:50:05 +0200

    Add files via upload

--- km-52/Vozniak_Anastasiia/project/code/create.sql
+++ km-52/Vozniak_Anastasiia/project/code/create.sql
@@ -1,132 +1,290 @@
-/*==============================================================*/
-/* DBMS name:      ORACLE Version 11g                           */
-/* Created on:     18.11.2018 22:23:43                          */
-/*==============================================================*/
 
+ALTER TABLE consultation
+   DROP CONSTRAINT fk_subject_consultation;
 
-alter table CONSULTATION
-   drop constraint FK_TEACHER_CONSULTATION;
+ALTER TABLE consultation
+   DROP CONSTRAINT fk_classroom_consultation;
 
-alter table CONSULTATION
-   drop constraint FK_CLASSROOM_CONSULTATION;
+ALTER TABLE consultation
+   DROP CONSTRAINT fk_user_consultation;
 
-alter table RECORD_ON_CONSULTATION
-   drop constraint FK_CONSULTATION_RECORD;
+ALTER TABLE RECORD
+   DROP CONSTRAINT fk_consultation_record;
 
-alter table RECORD_ON_CONSULTATION
-   drop constraint FK_STUDENT_RECORD;
+ALTER TABLE RECORD
+   DROP CONSTRAINT fk_user_record;
 
-alter table SCHEDULE_CONSULTATION
-   drop constraint FK_TEACHER_SCHEDULE;
+ALTER TABLE "USER"
+   DROP CONSTRAINT fk_role_user;
 
-drop table CLASSROOM cascade constraints;
+ALTER TABLE wishlist
+   DROP CONSTRAINT fk_subject_wishlist;
 
-drop table CONSULTATION cascade constraints;
+ALTER TABLE wishlist
+   DROP CONSTRAINT fk_user_wishlist;
 
-drop table RECORD_ON_CONSULTATION cascade constraints;
+DROP TABLE classroom CASCADE CONSTRAINTS;
 
-drop table SCHEDULE_CONSULTATION cascade constraints;
+DROP INDEX teacher_consults_student_fk;
 
-drop table STUDENT cascade constraints;
+DROP INDEX consultation_has_a_subject_fk;
 
-drop table TEACHER cascade constraints;
+DROP INDEX in_class_are_consultations_fk;
+
+DROP TABLE consultation CASCADE CONSTRAINTS;
+
+DROP INDEX students_make_records_fk;
+
+DROP INDEX records_on_consultation_fk;
+
+DROP TABLE RECORD CASCADE CONSTRAINTS;
+
+DROP TABLE ROLE CASCADE CONSTRAINTS;
+
+DROP TABLE subject CASCADE CONSTRAINTS;
+
+DROP INDEX user_has_a_role_fk;
+
+DROP TABLE "USER" CASCADE CONSTRAINTS;
+
+DROP INDEX on_subject_is_wishlist_fk;
+
+DROP INDEX student_has_a_wishlist_fk;
+
+DROP TABLE wishlist CASCADE CONSTRAINTS;
 
 /*==============================================================*/
 /* Table: CLASSROOM                                             */
 /*==============================================================*/
-create table CLASSROOM 
+CREATE TABLE classroom 
 (
-   CLASSROOM_NUMBER     VARCHAR2(5)          not null,
-   CLASSROOM_BUILDING   INTEGER              not null,
-   constraint PK_CLASSROOM primary key (CLASSROOM_NUMBER, CLASSROOM_BUILDING)
+   classroom_number     VARCHAR2(20)          NOT NULL,
+   classroom_building   INTEGER              NOT NULL,
+   CONSTRAINT pk_classroom PRIMARY KEY (classroom_number, classroom_building)
 );
 
 /*==============================================================*/
 /* Table: CONSULTATION                                          */
 /*==============================================================*/
-create table CONSULTATION 
+CREATE TABLE consultation 
 (
-   CONSULTATION_ID      INTEGER              not null,
-   TEACHER_ID_FK        INTEGER              not null,
-   CLASSROOM_NUMBER_FK  VARCHAR2(5)          not null,
-   CLASSROOM_BUILDING_FK INTEGER              not null,
-   CONSULTATION_DATE    DATE                 not null,
-   CONSULTATION_TIME    DATE                 not null,
-   CONSULTATION_TYPE    VARCHAR2(14)         not null,
-   CONSULTATION_SUBJECT VARCHAR2(100),
-   CONSULATION_DURATION INTEGER              not null,
-   constraint PK_CONSULTATION primary key (CONSULTATION_ID)
+   user_email_fk        VARCHAR2(150)         NOT NULL,
+   subject_name_fk      VARCHAR2(70)         NOT NULL,
+   classroom_number_fk  VARCHAR2(20)          NOT NULL,
+   classroom_building_fk INTEGER              NOT NULL,
+   consultation_begin   DATE                 NOT NULL,
+   consultation_end     DATE                 NOT NULL,
+   consultation_id      INTEGER              NOT NULL,
+   CONSTRAINT pk_consultation PRIMARY KEY (consultation_id)
 );
 
 /*==============================================================*/
-/* Table: RECORD_ON_CONSULTATION                                */
+/* Index: IN_CLASS_ARE_CONSULTATIONS_FK                         */
 /*==============================================================*/
-create table RECORD_ON_CONSULTATION 
-(
-   CONSULTATION_ID_FK     INTEGER              not null,
-   STUDENT_ID_FK        INTEGER              not null,
-   constraint PK_RECORD_ON_CONSULTATION primary key (CONSULTATION_ID, STUDENT_ID_FK)
+CREATE INDEX in_class_are_consultations_fk ON consultation (
+   classroom_number_fk ASC,
+   classroom_building_fk ASC
 );
 
 /*==============================================================*/
-/* Table: SCHEDULE_CONSULTATION                                 */
+/* Index: CONSULTATION_HAS_A_SUBJECT_FK                         */
 /*==============================================================*/
-create table SCHEDULE_CONSULTATION 
-(
-   SCHEDULE_CONSULT_DAY VARCHAR2(12)         not null,
-   TEACHER_ID_FK_1      INTEGER              not null,
-   SCHEDULE_CONSULT_START DATE                 not null,
-   SCHEDULE_CONSULT_END DATE                 not null,
-   constraint PK_SCHEDULE_CONSULTATION primary key (SCHEDULE_CONSULT_DAY, TEACHER_ID_FK_1)
+CREATE INDEX consultation_has_a_subject_fk ON consultation (
+   subject_name_fk ASC
 );
 
 /*==============================================================*/
-/* Table: STUDENT                                               */
+/* Index: TEACHER_CONSULTS_STUDENT_FK                           */
 /*==============================================================*/
-create table STUDENT 
-(
-   STUDENT_NAME         VARCHAR2(20)         not null,
-   STUDENT_SURNAME      VARCHAR2(20)         not null,
-   STUDENT_EMAIL        VARCHAR2(30)         not null,
-   STUDENT_GROUP        VARCHAR2(8)          not null,
-   STUDENT_ID           INTEGER              not null,
-   constraint PK_STUDENT primary key (STUDENT_ID)
+CREATE INDEX teacher_consults_student_fk ON consultation (
+   user_email_fk ASC
 );
 
 /*==============================================================*/
-/* Table: TEACHER                                               */
+/* Table: RECORD                                                */
 /*==============================================================*/
-create table TEACHER 
+CREATE TABLE RECORD 
 (
-   TEACHER_NAME         VARCHAR2(20)         not null,
-   TEACHER_SURNAME      VARCHAR2(20)         not null,
-   TEACHER_EMAIL        VARCHAR2(30)         not null,
-   TEACHER_FACULTY      VARCHAR2(5)          not null,
-   TEACHER_ID           INTEGER              not null,
-   constraint PK_TEACHER primary key (TEACHER_ID)
+   user_email_fk        VARCHAR2(150)         NOT NULL,
+   consultation_id_fk   INTEGER              NOT NULL,
+   consult_date         DATE                 NOT NULL,
+   record_id            INTEGER              NOT NULL,
+   CONSTRAINT pk_record PRIMARY KEY (record_id)
 );
 
-alter table CONSULTATION
-   add constraint FK_TEACHER_CONSULTATION foreign key (TEACHER_ID_FK)
-      references TEACHER (TEACHER_ID);
+/*==============================================================*/
+/* Index: RECORDS_ON_CONSULTATION_FK                            */
+/*==============================================================*/
+CREATE INDEX records_on_consultation_fk ON RECORD (
+   consultation_id_fk ASC
+);
 
-alter table CONSULTATION
-   add constraint FK_CLASSROOM_CONSULTATION foreign key (CLASSROOM_NUMBER_FK, CLASSROOM_BUILDING_FK)
-      references CLASSROOM (CLASSROOM_NUMBER, CLASSROOM_BUILDING);
+/*==============================================================*/
+/* Index: STUDENTS_MAKE_RECORDS_FK                              */
+/*==============================================================*/
+CREATE INDEX students_make_records_fk ON RECORD (
+   user_email_fk ASC
+);
 
-alter table RECORD_ON_CONSULTATION
-   add constraint FK_CONSULTATION_RECORD foreign key (CONSULTATION_ID_FK)
-      references CONSULTATION (CONSULTATION_ID);
+/*==============================================================*/
+/* Table: ROLE                                                  */
+/*==============================================================*/
+CREATE TABLE ROLE 
+(
+   user_role            VARCHAR2(30)         NOT NULL,
+   CONSTRAINT pk_role PRIMARY KEY(user_role)
+);
 
-alter table RECORD_ON_CONSULTATION
-   add constraint FK_STUDENT_RECORD foreign key (STUDENT_ID_FK)
-      references STUDENT (STUDENT_ID);
+/*==============================================================*/
+/* Table: SUBJECT                                               */
+/*==============================================================*/
+CREATE TABLE subject 
+(
+   subject_name         VARCHAR2(70)         NOT NULL,
+   CONSTRAINT pk_subject PRIMARY KEY (subject_name)
+);
 
-alter table SCHEDULE_CONSULTATION
-   add constraint FK_TEACHER_SCHEDULE foreign key (TEACHER_ID_FK_1)
-      references TEACHER (TEACHER_ID);
-	  
-	  
-	  
-	  
+/*==============================================================*/
+/* Table: "USER"                                                */
+/*==============================================================*/
+CREATE TABLE "USER" 
+(
+   user_role_fk         VARCHAR2(30)         NOT NULL,
+   user_name            VARCHAR2(80)         NOT NULL,
+   user_surname         VARCHAR2(80)         NOT NULL,
+   user_email           VARCHAR2(150)         NOT NULL,
+   user_password        VARCHAR2(80)         NOT NULL,
+   middle_name          VARCHAR2(80)         NOT NULL,
+   CONSTRAINT pk_user PRIMARY KEY (user_email)
+);
 
+/*==============================================================*/
+/* Index: USER_HAS_A_ROLE_FK                                    */
+/*==============================================================*/
+CREATE INDEX user_has_a_role_fk ON "USER" (
+   user_role_fk ASC
+);
+
+/*==============================================================*/
+/* Table: WISHLIST                                              */
+/*==============================================================*/
+CREATE TABLE wishlist 
+(
+   subject_name_fk1     VARCHAR2(70)         NOT NULL,
+   user_email_fk1       VARCHAR2(150)         NOT NULL,
+   wish_date            DATE                 NOT NULL,
+   wish_id              INTEGER              NOT NULL,
+   CONSTRAINT pk_wishlist PRIMARY KEY (wish_id)
+);
+
+/*==============================================================*/
+/* Index: STUDENT_HAS_A_WISHLIST_FK                             */
+/*==============================================================*/
+CREATE INDEX student_has_a_wishlist_fk ON wishlist (
+   user_email_fk1 ASC
+);
+
+/*==============================================================*/
+/* Index: ON_SUBJECT_IS_WISHLIST_FK                             */
+/*==============================================================*/
+CREATE INDEX on_subject_is_wishlist_fk ON wishlist (
+   subject_name_fk1 ASC
+);
+
+ALTER TABLE consultation
+   ADD CONSTRAINT fk_subject_consultation FOREIGN KEY (subject_name_fk)
+      REFERENCES subject (subject_name);
+
+ALTER TABLE consultation
+   ADD CONSTRAINT fk_classroom_consultation FOREIGN KEY (classroom_number_fk, classroom_building_fk)
+      REFERENCES classroom (classroom_number, classroom_building);
+
+ALTER TABLE consultation
+   ADD CONSTRAINT fk_user_consultation FOREIGN KEY (user_email_fk)
+      REFERENCES "USER" (user_email);
+
+ALTER TABLE RECORD
+   ADD CONSTRAINT fk_consultation_record FOREIGN KEY (consultation_id_fk)
+      REFERENCES consultation (consultation_id);
+
+ALTER TABLE RECORD
+   ADD CONSTRAINT fk_user_record FOREIGN KEY (user_email_fk)
+      REFERENCES "USER" (user_email);
+
+ALTER TABLE "USER"
+   ADD CONSTRAINT fk_role_user FOREIGN KEY (user_role_fk)
+      REFERENCES ROLE (user_role);
+
+ALTER TABLE wishlist
+   ADD CONSTRAINT fk_subject_wishlist FOREIGN KEY (subject_name_fk1)
+      REFERENCES subject (subject_name);
+
+ALTER TABLE wishlist
+   ADD CONSTRAINT fk_user_wishlist FOREIGN KEY (user_email_fk1)
+      REFERENCES "USER" (user_email);
+
+ALTER TABLE "USER"
+    ADD CONSTRAINT check_email 
+    CHECK (REGEXP_LIKE (user_email, '^[A-Z0-9._-]+@[A-Z0-9._-]+\.[A-Z]{2,4}'));
+
+ALTER TABLE "USER"
+    ADD CONSTRAINT check_password 
+	CHECK (REGEXP_LIKE(user_password, '^[0-9a-zA-Z]{6,20}'));
+	
+ALTER TABLE "USER"
+    ADD CONSTRAINT check_name 
+    CHECK (REGEXP_LIKE (user_name, '^[ЇІА-Я]{1}[іїа-я]{1,20}'));
+
+ALTER TABLE "USER"
+    ADD CONSTRAINT check_surname 
+    CHECK (REGEXP_LIKE (user_surname, '^[ЇІА-Я]{1}[іїа-я]{1,20}'));	
+	
+ALTER TABLE "USER"
+    ADD CONSTRAINT check_mid_name 
+    CHECK (REGEXP_LIKE (middle_name, '^[ЇІА-Я]{1}[іїа-я]{1,20}'));	
+	
+ALTER TABLE subject
+    ADD CONSTRAINT check_subject
+    CHECK (REGEXP_LIKE (subject_name, '^[-0-9a-zA-ZА-Яа-яЇїіІЙй\s]'));
+	
+ALTER TABLE classroom
+    ADD CONSTRAINT check_building
+    CHECK (classroom_building>0 AND classroom_building<40);
+
+ALTER TABLE wishlist
+    ADD CONSTRAINT check_wish
+    CHECK (wish_id>0 AND wish_id<1000);
+	
+ALTER TABLE consultation
+    ADD CONSTRAINT check_consult_id
+    CHECK (consultation_id>1001 AND consultation_id<2000); 
+
+ALTER TABLE RECORD
+    ADD CONSTRAINT check_record_id
+    CHECK (record_id>2001 AND record_id<3000); 	
+	
+ALTER TABLE consultation
+	ADD CONSTRAINT check_consult_date
+	CHECK (consultation_begin < consultation_end);
+	
+ALTER TABLE ROLE
+	ADD CONSTRAINT check_role_begin_end
+	CHECK (role_begin < role_end);		
+	
+ALTER TABLE classroom
+    ADD CONSTRAINT check_class 
+    CHECK (REGEXP_LIKE (classroom_number, '^[0-9]{1,3}[а-я]{0,1}'));
+	
+ALTER TABLE ROLE
+	ADD CONSTRAINT check_role
+	CHECK (user_role='Студент' OR  user_role='Викладач' OR  user_role='Адмін');
+		
+ALTER TABLE RECORD
+	ADD CONSTRAINT check_type
+	CHECK (consult_type='Індивідуальна' OR  consult_type='Групова');
+    
+ALTER TABLE RECORD 
+	ADD CONSTRAINT unique_user_consult
+	UNIQUE  (consultation_id_fk, user_email_fk);
+
+ALTER SESSION SET nls_date_format = 'DD-MM-YYYY HH24:MI';  
