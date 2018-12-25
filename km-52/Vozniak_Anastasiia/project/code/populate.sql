5751eb1f07690225e774885d94c4f97b6b65dfe2
Anastasiia13 <nastyavozniak@gmail.com>
2018-12-25 17:53:03 +0200

    Update populate.sql

--- km-52/Vozniak_Anastasiia/project/code/populate.sql
+++ km-52/Vozniak_Anastasiia/project/code/populate.sql
@@ -1,4 +1,6 @@
-Insert into CLASSROOM (CLASSROOM_NUMBER,CLASSROOM_BUILDING) values ('10','30');
+    ALTER SESSION SET nls_date_format = 'DD-MM-YYYY HH24:MI';   
+      
+    Insert into CLASSROOM (CLASSROOM_NUMBER,CLASSROOM_BUILDING) values ('10','30');
 Insert into CLASSROOM (CLASSROOM_NUMBER,CLASSROOM_BUILDING) values ('107','10');
 Insert into CLASSROOM (CLASSROOM_NUMBER,CLASSROOM_BUILDING) values ('10а','10');
 Insert into CLASSROOM (CLASSROOM_NUMBER,CLASSROOM_BUILDING) values ('5','5');
@@ -11,14 +13,14 @@ Insert into SUBJECT (SUBJECT_NAME) values ('Англійська мова');
 Insert into SUBJECT (SUBJECT_NAME) values ('БЖОП');
 Insert into SUBJECT (SUBJECT_NAME) values ('Вища математика');
 Insert into SUBJECT (SUBJECT_NAME) values ('Математичний аналіз');
-Insert into "USER" (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Студент','Федір','Федоров','fed123@gmail.com','fedfed','Федорович');
-Insert into "USER" (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Студент','Іван','Іванов','ivanov@mail.com','ivan111','Іванович');
-Insert into "USER" (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Викладач','Борис','Борисов','boris_1@gmail.com','123456','Борисович');
-Insert into "USER" (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Викладач','Сергій','Сергієнко','serg@gmail.com','789456','Сергійович');
-Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_FK1,WISH_DATE,WISH_ID) values ('Математичний аналіз','fed123@gmail.com',to_date('30-11-2018 14:40','DD-MM-YYYY HH24:MI'),'1');
-Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_FK1,WISH_DATE,WISH_ID) values ('Вища математика','ivanov@mail.com',to_date('01-12-2018 13:30','DD-MM-YYYY HH24:MI'),'2');
-Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_FK1,WISH_DATE,WISH_ID) values ('Вища математика','fed123@gmail.com',to_date('01-12-2018 13:30','DD-MM-YYYY HH24:MI'),'3');
-Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_FK1,WISH_DATE,WISH_ID) values ('Англійська мова','ivanov@mail.com',to_date('03-12-2018 10:00','DD-MM-YYYY HH24:MI'),'4');
+Insert into users (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Студент','Федір','Федоров','fed123@gmail.com','fedfed','Федорович');
+Insert into users (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Студент','Іван','Іванов','ivanov@mail.com','ivan111','Іванович');
+Insert into users (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Викладач','Борис','Борисов','boris_1@gmail.com','123456','Борисович');
+Insert into users (USER_ROLE_FK,USER_NAME,USER_SURNAME,USER_EMAIL,USER_PASSWORD,MIDDLE_NAME) values ('Викладач','Сергій','Сергієнко','serg@gmail.com','789456','Сергійович');
+Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_T,USER_EMAIL_S,WISH_DATE,WISH_ID) values ('Математичний аналіз','serg@gmail.com','fed123@gmail.com',to_date('30-11-2018 14:40','DD-MM-YYYY HH24:MI'),'1');
+Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_T,USER_EMAIL_S,WISH_DATE,WISH_ID) values ('Вища математика','serg@gmail.com','ivanov@mail.com',to_date('01-12-2018 13:30','DD-MM-YYYY HH24:MI'),'2');
+Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_T,USER_EMAIL_S,WISH_DATE,WISH_ID) values ('Вища математика','boris_1@gmail.com','fed123@gmail.com',to_date('01-12-2018 13:30','DD-MM-YYYY HH24:MI'),'3');
+Insert into WISHLIST (SUBJECT_NAME_FK1,USER_EMAIL_T,USER_EMAIL_S, WISH_DATE,WISH_ID) values ('Англійська мова','boris_1@gmail.com', 'ivanov@mail.com',to_date('03-12-2018 10:00','DD-MM-YYYY HH24:MI'),'4');
 Insert into CONSULTATION (USER_EMAIL_FK,SUBJECT_NAME_FK,CLASSROOM_NUMBER_FK,CLASSROOM_BUILDING_FK,CONSULTATION_BEGIN,CONSULTATION_END,CONSULTATION_ID) values ('boris_1@gmail.com','Математичний аналіз','5','5',to_date('01-12-2018 08:30','DD-MM-YYYY HH24:MI'),to_date('01-12-2018 10:00','DD-MM-YYYY HH24:MI'),'1002');
 Insert into CONSULTATION (USER_EMAIL_FK,SUBJECT_NAME_FK,CLASSROOM_NUMBER_FK,CLASSROOM_BUILDING_FK,CONSULTATION_BEGIN,CONSULTATION_END,CONSULTATION_ID) values ('boris_1@gmail.com','Вища математика','505','12',to_date('02-12-2018 12:20','DD-MM-YYYY HH24:MI'),to_date('02-12-2018 13:30','DD-MM-YYYY HH24:MI'),'1003');
 Insert into CONSULTATION (USER_EMAIL_FK,SUBJECT_NAME_FK,CLASSROOM_NUMBER_FK,CLASSROOM_BUILDING_FK,CONSULTATION_BEGIN,CONSULTATION_END,CONSULTATION_ID) values ('serg@gmail.com','БЖОП','107','10',to_date('02-12-2018 12:20','DD-MM-YYYY HH24:MI'),to_date('02-12-2018 13:55','DD-MM-YYYY HH24:MI'),'1004');
@@ -27,3 +29,5 @@ Insert into RECORD (USER_EMAIL_FK,CONSULTATION_ID_FK,CONSULT_DATE,RECORD_ID) val
 Insert into RECORD (USER_EMAIL_FK,CONSULTATION_ID_FK,CONSULT_DATE,RECORD_ID) values ('fed123@gmail.com','1003',to_date('02-12-2018 12:30','DD-MM-YYYY HH24:MI'),'2003');
 Insert into RECORD (USER_EMAIL_FK,CONSULTATION_ID_FK,CONSULT_DATE,RECORD_ID) values ('ivanov@mail.com','1003',to_date('02-12-2018 12:30','DD-MM-YYYY HH24:MI'),'2004');
 Insert into RECORD (USER_EMAIL_FK,CONSULTATION_ID_FK,CONSULT_DATE,RECORD_ID) values ('ivanov@mail.com','1004',to_date('02-12-2018 12:20','DD-MM-YYYY HH24:MI'),'2005');
+
+    
