f15bf72cc3e8f6f76a242c64908c929c088ca34a
Anastasiia13 <nastyavozniak@gmail.com>
2018-12-25 18:02:22 +0200

    Update record_package.sql

--- km-52/Vozniak_Anastasiia/workshop4/sql/record_package.sql
+++ km-52/Vozniak_Anastasiia/workshop4/sql/record_package.sql
@@ -1,11 +1,12 @@
-CREATE OR REPLACE PACKAGE record_package AS
-type record_table is record(
+create or replace PACKAGE record_package IS
+type rec_table is record(
     rec_date RECORD.CONSULT_DATE%type,
     u_email_t consultation.USER_EMAIL_FK%type,
     cons_subject consultation.SUBJECT_NAME_FK%TYPE,
     cons_class consultation.CLASSROOM_NUMBER_FK%type,
     cons_building consultation.CLASSROOM_BUILDING_FK%type);
-    
+type record_table is table of rec_table;
+
     FUNCTION get_records(
     u_email_s IN RECORD.USER_EMAIL_FK%type) 
     RETURN record_table
@@ -14,8 +15,7 @@ type record_table is record(
     PROCEDURE add_record(
     u_email_s IN record.USER_EMAIL_FK%type,
     cons_id IN record.CONSULTATION_ID_FK%type,
-    rec_date IN record.CONSULT_DATE%type,
-    rec_id in record.RECORD_ID%type
+    rec_date IN record.CONSULT_DATE%type
     );
 
     PROCEDURE del_rec (
@@ -26,76 +26,78 @@ type record_table is record(
     rec_id in record.RECORD_ID%type,
     rec_date IN record.CONSULT_DATE%type
     );
-    
-end record_package;
 
-   
-CREATE OR REPLACE PACKAGE BODY record_package AS
-    
+end record_package;
+/   
+create or replace PACKAGE BODY record_package IS
     FUNCTION get_records(
     u_email_s IN RECORD.USER_EMAIL_FK%type) 
     RETURN record_table
     PIPELINED
         is
-        create table consult_id as select CONSULTATION_ID_FK from RECORD
-        where RECORD.USER_EMAIL_FK = u_email_s;
         cursor rec_cur is 
-        select distinct
-        RECORD.CONSULT_DATE,
-        consultation.USER_EMAIL_FK,
-        consultation.SUBJECT_NAME_FK,
-        consultation.CLASSROOM_NUMBER_FK,
-        consultation.CLASSROOM_BUILDING_FK
-        from record inner join consult_id on 
-        record.CONSULTATION_ID_FK=consult_id.CONSULTATION_ID_FK
-        inner join consultation on record.CONSULTATION_ID_FK=consultation.CONSULTATION_ID;
-        
+        select 
+        record.consult_date,
+        consultation.user_email_fk,
+        SUBJECT_NAME_FK,
+        CLASSROOM_NUMBER_FK,
+        CLASSROOM_BUILDING_FK
+        from record
+        inner join consultation
+        on RECORD.USER_EMAIL_FK = u_email_s
+        and consultation.CONSULTATION_ID=record.CONSULTATION_ID_FK;
+
         begin
         for cur in rec_cur
         loop
             pipe row(cur);
         end loop;
-end get_records;
-    
-PROCEDURE add_record(
+    end get_records;
+
     PROCEDURE add_record(
     u_email_s IN record.USER_EMAIL_FK%type,
     cons_id IN record.CONSULTATION_ID_FK%type,
-    rec_date IN record.CONSULT_DATE%type,
-    rec_id in record.RECORD_ID%type
-    ) is
-    
+    rec_date IN record.CONSULT_DATE%type
+    )is
+    rec_id integer;
     begin
+    select max(record_id) into rec_id
+    from record;
     insert into record(
     USER_EMAIL_FK,
     CONSULTATION_ID_FK,
     CONSULT_DATE,
     RECORD_ID
     ) values(
-    u_email_s,
-    cons_id,
-    rec_date,
-    rec_id 
+   u_email_s,
+   cons_id,
+   rec_date,
+   rec_id+1
     );
-end add_record;
-    
-PROCEDURE del_rec(
-        rec_id in record.RECORD_ID%type
-    )is
-    begin
-        delete from record
-        where record_id = rec_id;
-end del_rec;
+    end add_record;
 
-PROCEDURE update_record(
+    PROCEDURE del_rec (
+         rec_id in record.RECORD_ID%type
+    ) is
+        begin
+        delete from record
+        where record_id =rec_id;
+
+    end del_rec;
+
+    PROCEDURE update_record (
     rec_id in record.RECORD_ID%type,
     rec_date IN record.CONSULT_DATE%type
-    ) IS BEGIN
+    )
+    IS BEGIN
     UPDATE record
         SET
-    CONSULT_DATE=rec_date
+    CONSULT_DATE = rec_date
     WHERE
-        RECORD_ID= rec_id;
-END update_record; 
+        record_id= rec_id;
+    end update_record;
 
-end record_package;    
+end record_package;  
+
+/
+
